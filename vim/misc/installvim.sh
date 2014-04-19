fae97b8dbd96dd014256882975a40a5a880fad23
ease79 <edreesen79@gmail.com>
2014-04-19 20:06:53 +0200

    replaced homedir name of a system with $PATH

--- vim/misc/installvim.sh
+++ vim/misc/installvim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-hg clone https://vim.googlecode.com/hg/ /home/classics/vim
-cd vim/src
+hg clone https://vim.googlecode.com/hg/ $HOME/vim
+cd $HOME/vim/src
 ./configure --enable-pythoninterp --with-features=huge -prefix=$HOME/opt/vim
 make
 make install
