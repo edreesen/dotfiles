bdaeb10c05a316568b0cc372237bffa1865649e8
ease79 <edreesen79@gmail.com>
2014-12-29 19:20:00 +0100

    updated th debian url for the speedtest

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -1,4 +1,4 @@
 alias ll='ls -la'
 alias lh='ls -lah'
 alias ...='cd ../..'
-alias speedtest='wget --report-speed=bits -O /dev/null http://ftp.informatik.uni-frankfurt.de/debian-cd/current/ia64/iso-cd/debian-7.5.0-ia64-netinst.iso'
+alias speedtest='wget --report-speed=bits -O /dev/null http://ftp.informatik.uni-frankfurt.de/debian-cd/current/ia64/iso-cd/debian-7.7.0-ia64-netinst.iso'
