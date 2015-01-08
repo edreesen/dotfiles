24d7e8fa839ad0ec1f447d769d706742b055c4b7
Robert Habermann <mail@rhab.de>
2015-01-08 23:03:54 +0100

    fixed symlink/dir clean up

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,31 +6,51 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc vimrc tmux.conf"    # list of files/folders to symlink in homedir
-
+dir=~/dotfiles                  # dotfiles directory
+olddir=~/dotfiles_old           # old dotfiles backup directory
+files="zshrc vimrc tmux.conf"   # list of files to symlink in homedir
+folders="vim zsh"               # list of folders to symlink in homedir
 ##########
 
 # create dotfiles_old in homedir
+if [[ -d $olddir ]];
+    then
+        echo "Removing old backup directory $olddir ..."
+        rm -rf $olddir
+        echo "done"
+fi
+
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     if [[ -f ~/.$file ]];
         then
-            echo "File ~/.$file exists. Moving to $olddir"
-            mv ~/.$file ~/dotfiles_old/
+            echo "File ~/.$file exists. Moving to $olddir."
+            mv ~/.$file $olddir
         else
             echo "File ~/.$file did not yet exist. No need to create backup."
         fi
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+for folder in $folders; do
+    if [[ -L ~/.$folder ]];
+        then
+            echo "Symlink named ~/.$folder exists. Removing."
+            rm -f ~/.$folder
+    fi
+
+    if [[ -d ~/.$folder ]];
+        then
+            echo "Folder ~/.$folder exists. Moving to $olddir."
+            mv ~/.$folder $olddir
+    fi
+
+    echo "Creating symlink to folder $folder in home directory."
+    ln -s $dir/$folder ~/.$folder
+done
+
