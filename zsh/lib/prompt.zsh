5fc48d223bc5abe63da6c5061ef319676804b9f7
Robert Habermann <mail@rhab.de>
2015-01-17 12:59:33 +0100

    move setting title from vimrc to prompt.zsh

--- zsh/lib/prompt.zsh
+++ zsh/lib/prompt.zsh
@@ -17,3 +17,32 @@ TRAPALRM() {
     zle reset-prompt
 }
 
+
+function title() {
+  # escape '%' chars in $1, make nonprintables visible
+  a=${(V)1//\%/\%\%}
+
+  # Truncate command, and join lines.
+  a=$(print -Pn "%40>...>$a" | tr -d "\n")
+
+  case $TERM in
+  screen)
+    print -Pn "\e]2;$a @ $2\a" # plain xterm title
+    print -Pn "\ek$a\e\\"      # screen title (in ^A")
+    print -Pn "\e_$2   \e\\"   # screen location
+    ;;
+  xterm*|rxvt)
+    print -Pn "\e]2;$a @ $2\a" # plain xterm title
+    ;;
+  esac
+}
+
+# precmd is called just before the prompt is printed
+function precmd() {
+  title "zsh" "%n@%M: %55<...<%~"
+}
+
+# preexec is called just before any command line is executed
+function preexec() {
+  title "$1" "%n@%M: %35<...<%~"
+}
