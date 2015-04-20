9cdb01702d580794dd1f4e3d1971b212179f73fb
Robert Habermann <mail@rhab.de>
2015-04-20 10:14:57 +0200

    add git configuration (with 3 way diff settings) to dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                  # dotfiles directory
 olddir=~/dotfiles_old           # old dotfiles backup directory
-files="zshrc vimrc tmux.conf"   # list of files to symlink in homedir
+files="zshrc vimrc tmux.conf gitconfig"   # list of files to symlink in homedir
 folders="vim zsh"               # list of folders to symlink in homedir
 ##########
 
