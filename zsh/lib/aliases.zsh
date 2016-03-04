c8f91d29edbb3ad842cc2e336e70a333a5a594dd
Robert Habermann <mail@rhab.de>
2016-03-04 22:59:47 +0100

    update speedtest to current URL

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -1,4 +1,4 @@
 alias ll='ls -la'
 alias lh='ls -lah'
 alias ...='cd ../..'
-alias speedtest='wget --report-speed=bits -O /dev/null http://ftp.informatik.uni-frankfurt.de/debian.org/debian-cd/current/amd64/iso-cd/debian-8.0.0-amd64-netinst.iso'
+alias speedtest='wget --report-speed=bits -O /dev/null http://ftp.informatik.uni-frankfurt.de/debian.org/debian-cd/current/amd64/iso-cd/debian-8.3.0-amd64-netinst.iso'
