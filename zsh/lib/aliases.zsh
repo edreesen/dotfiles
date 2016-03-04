6053e27052c762b20f31576200855dc9490a7311
Robert Habermann <mail@rhab.de>
2016-03-04 23:01:51 +0100

    add lr as ls -larS

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -1,4 +1,5 @@
 alias ll='ls -la'
 alias lh='ls -lah'
+alias lr='ls -lahrS'
 alias ...='cd ../..'
 alias speedtest='wget --report-speed=bits -O /dev/null http://ftp.informatik.uni-frankfurt.de/debian.org/debian-cd/current/amd64/iso-cd/debian-8.3.0-amd64-netinst.iso'
