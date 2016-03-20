b7fba8e29b1d61f01ccdb867a92d052c64312956
Robert Habermann <mail@rhab.de>
2016-03-20 18:33:26 +0100

    add alias to easly upgrade all pip packages

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -3,3 +3,4 @@ alias lh='ls -lah'
 alias lr='ls -lahrS'
 alias ...='cd ../..'
 alias speedtest='wget --report-speed=bits -O /dev/null http://ftp.informatik.uni-frankfurt.de/debian.org/debian-cd/current/amd64/iso-cd/debian-8.3.0-amd64-netinst.iso'
+alias pip_upgrade_all="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
