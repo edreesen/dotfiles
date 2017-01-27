7f5ffddda21ca53940ea889b83b70a965e83c134
Robert Habermann <mail@rhab.de>
2017-01-27 18:07:44 +0100

    add vim install info

---
+++ install_vim/README.md
@@ -0,0 +1,9 @@
+# install vim
+
+## install ncurses lib
+
+sudo yum install ncurses-devel
+
+sudo apt-get install libncurses5-dev (?)
+
+
---
+++ install_vim/installvim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+git clone https://github.com/vim/vim.git $HOME/vim
+cd $HOME/vim/src
+./configure --enable-pythoninterp --with-features=huge -prefix=$HOME/local/bin/vim
+make
+make install
+#mkdir -p $HOME/bin
+#cd $HOME/bin
+#ln -s $HOME/opt/vim/bin/vim
